6fc22a3dee552ae873cde40e8d7340c516dde318
Lormida <pocketbook.love24@gmail.com>
2023-02-04 23:21:01 +0300

    feat: add `handlers for unhandlerRejection & SIGTERM events`

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,25 @@ import logger from './utils/logger'
 const app = express()
 const PORT = config.get<number>('port')
 
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   logger.info(`Server is running at port: ${PORT}...`);
   await connectDatabase()
 }) 
+
+/**
+ * Handling critical error events
+ */
+process.on('unhandledRejection', (err: Error) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
+})
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully...')
+  server.close(() => {
+    console.log('Process terminated...')
+  })
+})
