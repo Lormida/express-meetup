5fe792aa30f8d062d242d2117d479a4f2e6e7886
Lormida <pocketbook.love24@gmail.com>
2023-02-05 22:50:15 +0300

    feat: add `meetup controller wireframe`

--- src/controllers/meetup.controller.ts
+++ src/controllers/meetup.controller.ts
@@ -1,21 +1,19 @@
 import { Request, Response } from 'express'
 import Meetup from '../model/meetup.model'
-import Role from '../model/role.model'
 
 class MeetupController {
   async getAllMeetups(req: Request, res: Response) {
     const allMeetups = await Meetup.find()
     return res.json(allMeetups)
+  }  
+  async getMeetupById(req: Request, res: Response) {
   }
   async createMeetup(req: Request, res: Response) {
-    const userRole = new Role({ value: 'USER' })
-    const adminRole = new Role({ value: 'ADMIN' })
-    const a = await userRole.save()
-    const b = await adminRole.save()
-
-    return res.json([a, b])
   }
-
+  async updateMeetupById(req: Request, res: Response) {
+  }
+  async deleteMeetupById(req: Request, res: Response) {
+  }
 }
 
 
