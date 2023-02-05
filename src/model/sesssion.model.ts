b38e035cf811b79066db7d98b3d12e0c5ce457e0
Lormida <pocketbook.love24@gmail.com>
2023-02-06 00:20:44 +0300

    feat(model): add `session model`

---
+++ src/model/sesssion.model.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+import { UserDocument } from './user.model';
+
+export interface SessionDocument extends mongoose.Document {
+  user: UserDocument["_id"];
+  refreshToken: string;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const SessionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  refreshToken: { type: String, required: true },
+  userAgent: { type: String },
+})
+
+const SessionModel = mongoose.model('Session', SessionSchema);
+export default SessionModel
