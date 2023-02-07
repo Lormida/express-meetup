2f48844cdc93deef60aae323503ef1b8d9c68c23
Lormida <pocketbook.love24@gmail.com>
2023-02-07 12:15:29 +0300

    chore: fix deps & alter .env

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
-  port: 3000,
+  port: process.env.PORT,
+  CLIENT_URL: process.env.CLIENT_URL,
   dbURI: 'mongodb+srv://lormida:midapa24@cluster0.oien5qs.mongodb.net/test',
-  CLIENT_URL: 'http://localhost:3000',
   JWT_ACCESS_SECRET: 'jwt-secret-key',
   JWT_REFRESH_SECRET: 'jwt-refresh-secret-key',
   ACCESS_TOKEN_EXPIRATION: '4h',
